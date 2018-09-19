737137a3dbbe4c5266c3f897232674488dd60690
Anne Gasté <anne.gaste@hotmail.fr>
2018-09-19 23:20:27 +0200

    modif img + bd

--- script/projet6.sql
+++ script/projet6.sql
@@ -1,35 +1,28 @@
--- phpMyAdmin SQL Dump
--- version 4.7.9
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
--- Hôte : 127.0.0.1:3306
--- Généré le :  jeu. 26 juil. 2018 à 20:06
--- Version du serveur :  5.7.21
--- Version de PHP :  7.2.4
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: projet6
+-- ------------------------------------------------------
+-- Server version	5.7.21
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Base de données :  `projet6`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `annonces`
+-- Table structure for table `annonces`
 --
 
 DROP TABLE IF EXISTS `annonces`;
-CREATE TABLE IF NOT EXISTS `annonces` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `annonces` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
   `content` text NOT NULL,
@@ -39,54 +32,54 @@ CREATE TABLE IF NOT EXISTS `annonces` (
   `city` varchar(255) NOT NULL,
   `author` varchar(255) NOT NULL,
   `creation_date` datetime NOT NULL,
+  `spam` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=45 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=53 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Déchargement des données de la table `annonces`
+-- Dumping data for table `annonces`
 --
 
-INSERT INTO `annonces` (`id`, `title`, `content`, `typeof`, `tel`, `email`, `city`, `author`, `creation_date`) VALUES
-(5, 'bricolage', 'blablablablabalbalbalbalabalblabalblabalbalab', 'recherche', '0606060606', 'a@hotmail.fr', 'ploucville', 'heyhey', '2018-07-26 23:13:20'),
-(4, 'bricolage', 'blablablablabalbalbalbalabalblabalblabalbalab', 'recherche', '0606060606', 'a@hotmail.fr', 'ploucville', 'heyhey', '2018-07-26 23:13:20'),
-(3, 'bricolage', 'blablablablabalbalbalbalabalblabalblabalbalab', 'recherche', '0606060606', 'a@hotmail.fr', 'ploucville', 'heyhey', '2018-07-26 23:13:20'),
-(2, 'bricolage', 'blablablablabalbalbalbalabalblabalblabalbalab', 'recherche', '0606060606', 'a@hotmail.fr', 'ploucville', 'heyhey', '2018-07-26 23:13:20'),
-(1, 'bricolage', 'blablablablabalbalbalbalabalblabalblabalbalab', 'recherche', '0606060606', 'a@hotmail.fr', 'ploucville', 'heyhey', '2018-07-26 23:13:20'),
-(6, 'bricolage', 'blablablablabalbalbalbalabalblabalblabalbalab', 'recherche', '0606060606', 'a@hotmail.fr', 'ploucville', 'heyhey', '2018-07-26 23:13:20'),
-(7, 'bricolage', 'blablablablabalbalbalbalabalblabalblabalbalab', 'propose', '0606060606', 'a@hotmail.fr', 'ploucville', 'heyhey', '2018-07-26 23:13:20'),
-(8, 'bricolage', 'blablablablabalbalbalbalabalblabalblabalbalab', 'propose', '0606060606', 'a@hotmail.fr', 'ploucville', 'heyhey', '2018-07-26 23:13:20'),
-(9, 'bricolage', 'blablablablabalbalbalbalabalblabalblabalbalab', 'propose', '0606060606', 'a@hotmail.fr', 'ploucville', 'heyhey', '2018-07-26 23:13:20'),
-(10, 'bricolage', 'blablablablabalbalbalbalabalblabalblabalbalab', 'propose', '0606060606', 'a@hotmail.fr', 'ploucville', 'heyhey', '2018-07-26 23:13:20'),
-(11, 'bricolage', 'blablablablabalbalbalbalabalblabalblabalbalab', 'propose', '0606060606', 'a@hotmail.fr', 'ploucville', 'heyhey', '2018-07-26 23:13:20'),
-(12, 'bricolage', 'blablablablabalbalbalbalabalblabalblabalbalab', 'propose', '0606060606', 'a@hotmail.fr', 'ploucville', 'heyhey', '2018-07-26 23:13:20');
+LOCK TABLES `annonces` WRITE;
+/*!40000 ALTER TABLE `annonces` DISABLE KEYS */;
+INSERT INTO `annonces` VALUES (49,'','','recherche','','','','','2018-09-18 15:36:35',NULL),(52,'test','blabla','propose','0643007965','anne.gaste@hotmail.fr','corbeil-essonnes','bibi','2018-09-19 22:33:32',NULL);
+/*!40000 ALTER TABLE `annonces` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
--- --------------------------------------------------------
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
---
--- Structure de la table `member`
+-- Table structure for table `member`
 --
 
 DROP TABLE IF EXISTS `member`;
-CREATE TABLE IF NOT EXISTS `member` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `member` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `pseudo` varchar(255) NOT NULL,
   `pass` text NOT NULL,
   `email` varchar(255) NOT NULL,
   `date_inscription` date NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Déchargement des données de la table `member`
+-- Dumping data for table `member`
 --
 
-INSERT INTO `member` (`id`, `pseudo`, `pass`, `email`, `date_inscription`) VALUES
-(10, 'leeloo', 'test', 'anne.gaste@hotmail.fr', '2018-07-25'),
-(11, 'blabla', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3', 'blabl@hotmail.fr', '2018-07-25'),
-(12, 'toto', '0b9c2625dc21ef05f6ad4ddf47c5f203837aa32c', 'toto@hotmail.fr', '2018-07-25'),
-(13, 'test', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3', 'test@hotmail.fr', '2018-07-25');
-COMMIT;
+LOCK TABLES `member` WRITE;
+/*!40000 ALTER TABLE `member` DISABLE KEYS */;
+INSERT INTO `member` VALUES (14,'leeloo','$2y$10$uyXBAlbLxOF14uaHyCcP.u5uIqXsuJE/hGAOzS4ptPHF10D6O5ki.','anne.gaste@hotmail.fr','2018-09-13'),(15,'leeloo','$2y$10$n7cTTdJPCI1xStdB/weP0ujJZpxn023AI9kqNZIS/ENfDVM8R0dLC','master@hotmail.fr','2018-09-17'),(16,'leeloo','$2y$10$OrDqyeB/FbbFQeo2CAhPIu7wgXX93R1utzs48RctuPRB0QDyAn6QS','master2@hotmail.fr','2018-09-17'),(17,'leeloo','$2y$10$LFzIJ2PZZPwQ9ftrysc6D.fw0NwHOJSHe7xOxnhhjQfvp3TnoDHt6','anne.gaste@hotmail.fr','2018-09-19');
+/*!40000 ALTER TABLE `member` ENABLE KEYS */;
+UNLOCK TABLES;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- Dump completed on 2018-09-19 23:15:57
